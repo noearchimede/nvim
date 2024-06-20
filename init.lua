3827f3aa7a35521dd7f03ba96b0a9e9a06ed160e
Noè Archimede Pezzoli <noearchimede@gmail.com>
2024-06-20 20:54:17 +0200

    added keyboard_standardization plugin

--- init.lua
+++ init.lua
@@ -22,3 +22,11 @@ require("mappings")
 require("settings")
 
 
+-- – Additional settings ––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+
+-- custom keyboard standardization script
+vim.g.vimrc_keyboard_remappings_path = vim.fn.stdpath("config") .. "/.vimrc_keyboard_standardization.vim"
+if vim.fn.filereadable(vim.fn.expand(vim.g.vimrc_keyboard_remappings_path)) ~= 0 then
+    vim.cmd('source ' ..  vim.g.vimrc_keyboard_remappings_path)
+end

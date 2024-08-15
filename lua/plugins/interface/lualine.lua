2a4a3401e7b2e683ad4809cb92a627844c406561
Noè Archimede Pezzoli <noearchimede@gmail.com>
2024-08-15 17:26:11 +0200

    Added status bar plugins: Lualine and Tabby

---
+++ lua/plugins/interface/lualine.lua
@@ -0,0 +1,71 @@
+return {
+
+    'nvim-lualine/lualine.nvim',
+
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+    },
+
+    init = function()
+        -- always display tabline, even with only one tab open
+        vim.opt.showtabline = 2
+    end,
+
+    opts = {
+        options = {
+            icons_enabled = false,
+            theme = 'auto',
+            component_separators = { left = '', right = ''},
+            section_separators = { left = '', right = ''},
+            disabled_filetypes = {
+                statusline = { 'NvimTree' },
+                winbar = {},
+            },
+            ignore_focus = {},
+            always_divide_middle = false,
+            globalstatus = false,
+            refresh = {
+                statusline = 1000,
+                tabline = 1000,
+                winbar = 1000,
+            }
+        },
+
+        sections = {
+            lualine_a = { 'mode'},
+            lualine_b = { 'branch' , 'diff', 'diagnostics'},
+            lualine_c = { {
+                'filename',
+                path = 1,
+                shorting_target = 40,
+                symbols = { modified = '●' },
+            } },
+            lualine_x = { 'encoding' , 'filetype'},
+            lualine_y = { 'progress' , 'searchcount'},
+            lualine_z = { 'location' }
+        },
+
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { {
+                'filename',
+                path = 1,
+                shorting_target = 40,
+                symbols = { modified = '●' },
+            } },
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {}
+        },
+
+        tabline = {},
+
+        winbar = {},
+        inactive_winbar = {},
+
+        extensions = {}
+
+    }
+
+}

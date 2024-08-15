7933b020c2682d0687cd9681860d0f61e2b6d654
Noè Archimede Pezzoli <noearchimede@gmail.com>
2024-08-15 17:18:01 +0200

    Added Lazy.nvim

--- init.lua
+++ init.lua
@@ -7,13 +7,33 @@
 -- – Leader key –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 vim.g.mapleader = '\\'
-vim.g.maplocalleader = '\\'
+vim.g.maplocalleader = ','
 
 
 
 -- – Plugins ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-require("plugins")
+-- disable netrw (nvim-tree is used instead)
+-- this needs to be called at the start of the configuration
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Plugins managed by lazy-nvim. The actual plugin specs are defined in the
+-- 'plugins' folder, where each plugin or group of plugins is defined in its
+-- own file.
+-- install lazy.nvim (code copied from lazy.nvim documentation)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- load plugins
+require("lazy").setup("plugins")
+
 
 
 -- – Main global settings –––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -22,8 +42,8 @@ require("mappings")
 require("settings")
 
 
--- – Additional settings ––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
+-- – Additional settings ––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 -- custom keyboard standardization script
 vim.g.vimrc_keyboard_remappings_path = vim.fn.stdpath("config") .. "/.vimrc_keyboard_standardization.vim"

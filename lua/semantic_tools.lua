42664dee20daa14b1c13eca5ac8089bd758f5b51
Noè Archimede Pezzoli <noearchimede@gmail.com>
2024-08-19 12:42:50 +0200

    LSP: added config for lua_ls

--- lua/semantic_tools.lua
+++ lua/semantic_tools.lua
@@ -53,10 +53,31 @@ M.lsp_settings = function(lspconfig, capabilities)
                 capabilities = capabilities,
                 settings = {
                     Lua = {
-                        -- make the language server recognize "vim" global
-                        diagnostics = {
-                            globals = { "vim" },
+                        workspace = {
+                            library = {
+                                -- make the language server recognize "vim" global
+                                vim.env.VIMRUNTIME
+                            }
                         },
+                        format = {
+                            -- for defaults and a list of options see:
+                            -- https://github.com/CppCXY/EmmyLuaCodeStyle/blob/master/lua.template.editorconfig
+                            defaultConfig = {
+
+                                -- disable most alignment settings that are enable by default
+                                align_continuous_assign_statement = "false",
+                                align_continuous_rect_table_field = "false",
+                                align_array_table = "false",
+                                align_continuous_inline_comment = "false",
+                                -- enable alignemt of multiline function arguments under first argument
+                                align_call_args = "true",
+
+                                -- always prevent the formatter from adding/removing blank lines
+                                line_space_after_function_statement = "keep",
+                                line_space_around_block = "keep",
+
+                            }
+                        }
                     },
                 }
             })

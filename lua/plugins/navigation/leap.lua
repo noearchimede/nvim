ff26fefd3b94a2607455e670bf6535d73c81ad6b
Noè Archimede Pezzoli <noearchimede@gmail.com>
2024-08-21 16:39:35 +0200

    Leap: added comment (explain why not bidirectional)

--- lua/plugins/navigation/leap.lua
+++ lua/plugins/navigation/leap.lua
@@ -6,6 +6,7 @@ return {
 
     config = function()
         -- those are the default mappings (s: forward, S: backward, gs: leap from window)
+        -- note: bidirectional leaping is possible but removes
         vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
         vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
         vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')

6b67ea3af79e072e84b027f929c61ed16bf709b7
Noè Archimede Pezzoli <noearchimede@gmail.com>
2024-08-24 22:59:23 +0200

    tree: minor options update

--- lua/plugins/files/tree.lua
+++ lua/plugins/files/tree.lua
@@ -216,34 +216,26 @@ return {
         view = {
             cursorline = true,
             preserve_window_proportions = true,
-            -- There are issues with this setting both enabled and disabled.
+            -- ^^^ There are issues with this setting both enabled and disabled.
             -- Problem when enabled: open two vertical splits of same width, open tree -> the rhs one will become way too narrow
             -- Problem when disabled: open two unequally sized vertical splits, open tree, make a diagnostic error appear in one buffer -> the buffer sizes will equalize
             width = {
-                min = 20,
-                max = 40,
+                min = 15,
+                max = 35,
                 padding = 1,
             },
         },
         renderer = {
-            group_empty = false,
-            full_name = false,
-            indent_width = 2,
-            special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+            special_files = { "README.md", "readme.md" },
+            hidden_display = "simple",
             symlink_destination = false,
-            highlight_git = "none",
-            highlight_diagnostics = "none",
-            highlight_opened_files = "none",
-            highlight_modified = "none",
-            highlight_bookmarks = "none",
-            highlight_clipboard = "name",
             icons = {
                 web_devicons = {
                     file = { enable = false, },
                     folder = { enable = false, },
                 },
-                git_placement = "after",
-                modified_placement = "before",
+                git_placement = "before",
+                modified_placement = "after",
                 diagnostics_placement = "signcolumn",
                 bookmarks_placement = "signcolumn",
                 padding = " ",
@@ -289,9 +281,6 @@ return {
             update_root = {
                 enable = false,
             },
-        },
-        system_open = {
-            cmd = "",
         },
         git = {
             enable = true,

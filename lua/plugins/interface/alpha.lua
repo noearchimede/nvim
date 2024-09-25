e07c92377172031b240779962137eeca33994bef
Noè Archimede Pezzoli <noearchimede@gmail.com>
2024-09-25 22:26:34 +0200

    alpha: updated startup screen

--- lua/plugins/interface/alpha.lua
+++ lua/plugins/interface/alpha.lua
@@ -8,67 +8,126 @@ return {
 
     config = function()
 
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.dashboard")
-
-        -- Set header
-        dashboard.section.header.val = {
-            [[                               __                ]],
-            [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-            [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-            [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-            [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-            [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-            '',
-            '',
-            '',
+        local colors = {
+            header = "ErrorMsg",
+            icon = "Constant",
+            action = "Normal",
+            shortcut = "Special",
+            footer_1 = "Number",
+            footer_2 = "Delimiter",
         }
+        local buttons_width = 35
 
-        dashboard.section.buttons.val = {
-            dashboard.button("n", "   New file", "<cmd>enew<CR>"),
-            dashboard.button("w", "   Workspaces", "<cmd>Telescope workspaces<CR>"),
-            dashboard.button("s", "   Sessions", "<cmd>Telescope possession<CR>"),
-            dashboard.button("f", "󰮗   Find file", "<cmd>Telescope find_files<CR>"),
-            dashboard.button("r", "   Recent files", "<cmd>Telescope oldfiles<CR>"),
-            dashboard.button("o", "   Oil", "<cmd>Oil<CR>"),
-            dashboard.button("c", "   Configuration", "<cmd>cd ~/.config/nvim <bar> edit README.md<CR>"),
-            dashboard.button("q", "󰗼   Quit", "<cmd>quit<CR>"),
-        }
-
-        -- helper function to center-align multiple lines of text in the 'footer' section
-        local function center_align(text_table)
-            local max_length = 0
-            for _, text in ipairs(text_table) do max_length = math.max(max_length, #text) end
-            local ret = {}
-            for _, text in ipairs(text_table) do
-                local padding = math.floor((max_length - #text) / 2)
-                table.insert(ret, string.rep(" ", padding) .. text)
-            end
-            return ret
+        local function text(hl, text_table)
+            return {
+                type = "text",
+                val = text_table,
+                opts = {
+                    position = "center",
+                    hl = hl
+                }
+            }
         end
 
-        local vers = vim.version()
-        dashboard.section.footer.val = center_align({
-            '',
-            '',
-            '',
-            os.date("%A %d %B %Y"),
-            os.date("%H:%M"),
-            '',
-            '',
-            "NVIM v" .. vers.major .. "." .. vers.minor .. "." .. vers.patch,
+        local function button(key, icon, desc, command)
+            local action = function() vim.cmd(command) end
+            return {
+                type = "button",
+                val = icon .. "   " .. desc,
+                on_press = action,
+                opts = {
+                    position = "center",
+                    hl = {
+                        { colors.icon, 0, 3 },
+                        { colors.action, 4, -1 },
+                    },
+                    keymap = { "n", key, action, { silent = true } },
+                    align_shortcut = "right",
+                    shortcut = key,
+                    hl_shortcut = colors.shortcut,
+                    cursor = 2, -- place cursor on this column
+                    width = buttons_width,
+                    shrink_margin = true
+                }
+            }
+        end
+
+        local function padding(lines)
+            return {
+                type = "padding",
+                val = lines
+            }
+        end
+
+        local function group(spacing, items)
+            return {
+                type = "group",
+                val = items,
+                opts = {
+                    spacing = spacing
+                }
+            }
+        end
+
+        -- alpha layout
+        local layout = {
+            padding(3),
+            text(colors.header, {
+                '',
+                [[      __                _            ]],
+                [[   /\ \ \___  ___/\   /(_)_ __ ___   ]],
+                [[  /  \/ / _ \/ _ \ \ / | | '_ ` _ \  ]],
+                [[ / /\  |  __| (_) \ V /| | | | | | | ]],
+                [[ \_\ \/ \___|\___/ \_/ |_|_| |_| |_| ]],
+                ''
+            }),
+            padding(1),
+            text(colors.footer_1, {
+                os.date("%A, %d %B %Y"),
+                --os.date("%H:%M"),
+            }),
+            padding(3),
+            group(1, {
+                button("n", "", "New file", "enew"),
+                button("w", "", "Workspaces", "Telescope workspaces"),
+                button("s", "󰦖", "Sessions", "Telescope possession"),
+                button("l", "", "Restore latest session", "PSLoad"),
+                button("f", "󰮗", "Find file", "Telescope find_files"),
+                button("r", "", "Recent files", "Telescope oldfiles"),
+                button("t", "", "File tree", "NvimTreeOpen"),
+                button("o", "󰍜", "Oil", "Oil"),
+                button("c", "", "Configuration", "cd ~/.config/nvim <bar> edit README.md"),
+                button("h", "?", "Help", "vert help config.txt"),
+                button("q", "󰗼", "Quit", "quit"),
+            }),
+            padding(4),
+            text(colors.footer_2,
+                "NVIM v" .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch)
+        }
+
+        -- alpha options
+        local opts = {
+            margin = 5,
+        }
+
+        -- Send config to alpha
+        require("alpha").setup({
+            layout = layout,
+            opts = opts,
         })
 
-        -- create autocmd to hide statusline and tabline
+    end,
+
+    init = function()
+
+        -- create autocmd to set local settings
         vim.api.nvim_create_autocmd("FileType", {
             pattern = "alpha",
             callback = function()
                 vim.opt_local.foldenable = false
+                vim.opt_local.wrap = false
             end
         })
 
-        -- Send config to alpha
-        alpha.setup(dashboard.opts)
-
-    end,
+    end
 }

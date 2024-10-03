a2758fe128ce76f4d9fe3aa7a22ec4715cebeead
Noè Archimede Pezzoli <noearchimede@gmail.com>
2024-10-03 19:44:45 +0200

    quicker: removed autocmd to force quickfix to bottom

--- lua/plugins/navigation/quicker.lua
+++ lua/plugins/navigation/quicker.lua
@@ -28,21 +28,6 @@ return {
         max_filename_width = function()
             return math.floor(math.min(80, vim.o.columns / 3))
         end,
-    },
-
-    init = function()
-
-        -- create autocmd to force the quickfix list to be as wide as the screen
-        -- adapted from: https://github.com/fatih/vim-go/issues/1757#issuecomment-565130503
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = 'qf',
-            callback = function()
-                if vim.fn.getwininfo(vim.fn.win_getid()).loclist ~= 1 then
-                    vim.cmd('wincmd J')
-                end
-            end
-        })
-
-    end
+    }
 
 }

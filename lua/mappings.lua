b87dc06ae1057dd226786952fef03ee170755499
Noè Archimede Pezzoli <noearchimede@gmail.com>
2024-10-18 14:23:54 +0200

    lspconfig: moved LSP mappings from lspconfig.lua to mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,7 @@
 --
 --   Leader
 --   Text Navigation
+--   LSP
 --   Editor Navigation
 --   Searching
 --   Editing
@@ -40,6 +41,65 @@ vim.keymap.set({ 'n', 'v', 'x', 'o' }, '\'', '`', { remap = true })
 vim.keymap.set({ 'n', 'v' }, '<leader>mj', ":<C-u>call search('\\%' . virtcol('.') . 'v\\S', 'W')<CR>")
 vim.keymap.set({ 'n', 'v' }, '<leader>mk', ":<C-u>call search('\\%' . virtcol('.') . 'v\\S', 'bW')<CR>")
 
+
+-- – LSP --------–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+-- create autocommand for LSP attach to define (buffer-local) mappings etc.
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = 'LSP actions',
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(event)
+
+        local function map(mode, key, func, desc)
+            vim.keymap.set(mode, key, func, { desc = desc, buffer = event.buf, silent = true })
+        end
+
+        -- Insert mode mappings
+        map({ "i", "n" }, "<C-s>", vim.lsp.buf.signature_help, "LSP: show signature help")
+
+        -- Normal and Visual mode mappings
+        map("n", "<leader><leader>n", vim.diagnostic.goto_next, "Diagnostics: next")
+        map("n", "<leader><leader>p", vim.diagnostic.goto_prev, "Diagnostics: previous")
+        map("n", "]d", vim.diagnostic.goto_next, "Diagnostics: next") -- overwrite :h ]d-default to also show the diagnostic text
+        map("n", "[d", vim.diagnostic.goto_prev, "Diagnostics: previous") -- see above
+        map("n", "]e", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end,
+            "Diagnostics: next error")
+        map("n", "[e", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end,
+            "Diagnostics: previous error")
+        map("n", "]w", function() vim.diagnostic.goto_next({ severity = { min = vim.diagnostic.severity.WARN } }) end,
+            "Diagnostics: next error or warning")
+        map("n", "[w", function() vim.diagnostic.goto_prev({ severity = { min = vim.diagnostic.severity.WARN } }) end,
+            "Diagnostics: previous error or warning")
+
+        map("n", "<leader><leader>v", vim.diagnostic.open_float, "Diagnostics: show line diagnostics")
+        map("n", "<leader><leader>k", vim.lsp.buf.hover, "LSP: show documentation hover") -- equivalent to K, see :h K-lsp-default
+        map("n", "<leader><leader>j", vim.lsp.buf.signature_help, "LSP: show signature help")
+        map("n", "<leader><leader>d", vim.lsp.buf.definition, "LSP: go to definition")
+        map("n", "<leader><leader>l", vim.lsp.buf.declaration, "LSP: go to declaration")
+        map("n", "<leader><leader>i", vim.lsp.buf.implementation, "LSP: go to implementation")
+        map("n", "<leader><leader>t", vim.lsp.buf.type_definition, "LSP: go to type definition")
+        map("n", "<leader><leader>r", vim.lsp.buf.references, "LSP: go to references")
+
+        map("n", "<leader><leader>D", function() require('utils').lsp_goto_with_picker(vim.lsp.buf.definition) end,
+            "LSP: go to definition")
+        map("n", "<leader><leader>L", function() require('utils').lsp_goto_with_picker(vim.lsp.buf.declaration) end,
+            "LSP: go to declaration")
+        map("n", "<leader><leader>I", function() require('utils').lsp_goto_with_picker(vim.lsp.buf.implementation) end,
+            "LSP: go to implementation")
+        map("n", "<leader><leader>T", function() require('utils').lsp_goto_with_picker(vim.lsp.buf.type_definition) end,
+            "LSP: go to type definition")
+
+        map("n", "<leader><leader>c", vim.lsp.buf.rename, "LSP: rename")
+        map({ "n", "v" }, "<leader><leader>a", vim.lsp.buf.code_action, "LSP: code actions")
+        map({ "n", "v" }, "<leader><leader>f", vim.lsp.buf.format, "LSP: format")
+
+        -- stop/start/restart LPS
+        map("n", "<leader><leader>qq", ":LspStop<CR>", "LSP: stop")
+        map("n", "<leader><leader>qr", ":LspRestart<CR>", "LSP: restart")
+        map("n", "<leader><leader>qs", ":LspStart<CR>", "LSP: restart")
+
+    end
+})
 
 
 -- – Quickfix and location list ––––––––––––––––––––––––––––––––––––––––––––––––
@@ -82,7 +142,7 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.keymap.set('n', 'q', "<cmd>close<cr>", { buffer = true, desc = "Close" })
         vim.keymap.set('n', 'o', "<cr><c-w>p", { buffer = true, desc = "Jump but keep focus" })
         vim.keymap.set('n', '<cr>', "<cr>", { buffer = true, desc = "Jump" })
-        
+
         -- motions
         local function motion(key)
             if vim.g.quickfix_auto_jump == true then
@@ -94,7 +154,8 @@ vim.api.nvim_create_autocmd("FileType", {
         end
 
         -- note: 'not nil' is 'true', so this works even if the control variable is not yet set
-        vim.keymap.set('n', 'P', function() vim.g.quickfix_auto_jump = not vim.g.quickfix_auto_jump end, { buffer = true, desc = "Toggle autojump" })
+        vim.keymap.set('n', 'P', function() vim.g.quickfix_auto_jump = not vim.g.quickfix_auto_jump end,
+            { buffer = true, desc = "Toggle autojump" })
         vim.keymap.set('n', 'j', function() motion('j') end, { buffer = true, desc = "Next" })
         vim.keymap.set('n', 'k', function() motion('k') end, { buffer = true, desc = "Previous" })
         vim.keymap.set('n', 'gg', function() motion('gg') end, { buffer = true, desc = "First" })

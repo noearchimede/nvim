69e4b8927bac07eeca829fbdea2e834ea1964761
Noè Archimede Pezzoli <noearchimede@gmail.com>
2025-05-21 23:11:59 +0200

    config-local: removed lazy loading

--- lua/plugins/workspace/config-local.lua
+++ lua/plugins/workspace/config-local.lua
@@ -2,8 +2,6 @@ return {
 
     "klen/nvim-config-local",
 
-    event = "VeryLazy",
-
     opts = {
       config_files = { ".nvim.lua", ".nvimrc", ".exrc" }, -- these three filenames are supported natively by ':h exrc'
       lookup_parents = true, -- Lookup config files in parent directories

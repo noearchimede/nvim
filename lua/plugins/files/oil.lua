79dd282d9eff41ef481fe7f2b8a870d3a780d882
Noè Archimede Pezzoli <noearchimede@gmail.com>
2025-07-31 23:21:30 +0200

    oil: update config

--- lua/plugins/files/oil.lua
+++ lua/plugins/files/oil.lua
@@ -7,39 +7,39 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
 
     keys = {
-        { "<leader>to", "<cmd>Oil<cr>", { desc = "Open Oil in current buffer" } }
+        { "<leader>to", "<cmd>Oil<cr>", { desc = "Open Oil" } },
+        { "-", "<cmd>Oil<cr>", { desc = "Open Oil" } }
     },
 
     opts = {
-
         -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
-        -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
         default_file_explorer = true,
-
         -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
         delete_to_trash = true,
-
         -- Oil will automatically delete hidden buffers after this delay
         cleanup_delay_ms = 5000, -- default: 2000
-
         -- Set to true to watch the filesystem for changes and reload oil
         watch_for_changes = true,
-
         -- Keymaps (only different than defaults are listed)
         keymaps = {
-            -- default is 's' for actions.select vertical
+            -- for consistency with other plugins
+            ["<C-h>"] = false, -- I use this to switch windows
+            ["<C-l>"] = false, -- I use this to switch windows
             ["<C-v>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
-            -- default is <c-l>
+            ["<C-x>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a vertical split" },
             ["<C-r>"] = "actions.refresh",
-        },
-
-        view_options = {
-            -- Show files and directories that start with "."
-            show_hidden = false, -- can toggle with g. by default
-            -- This function defines what will never be shown, even when `show_hidden` is set
-            is_always_hidden = function(name, bufnr)
-                return string.match(name, '.DS_Store')
-            end,
+            ["<C-s>"] = function()
+                local ok, grug = pcall(require, 'grug-far')
+                if ok then
+                    local entry = require('oil').get_cursor_entry()
+                    local dir = require('oil').get_current_dir()
+                    grug.open({
+                        windowCreationCommand = 'tab split',
+                        openTargetWindow = { preferredLocation = 'right' },
+                        prefills = { paths = dir .. entry.name },
+                    })
+                end
+            end
         },
     }
 

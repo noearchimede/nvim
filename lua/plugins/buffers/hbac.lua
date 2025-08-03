f3515f8dbaff698cc33d8e0e8e82c75042b4aaf3
Noè Archimede Pezzoli <noearchimede@gmail.com>
2025-08-04 00:06:24 +0200

    hbac: simplify config (minor)

--- lua/plugins/buffers/hbac.lua
+++ lua/plugins/buffers/hbac.lua
@@ -8,31 +8,20 @@ return {
         {
             "<leader>bu",
             function()
-                vim.ui.input({ prompt = "HBAC: close all unpinned buffers? [y/N]" }, function(input)
+                vim.ui.input({ prompt = "HBAC: close all untouched buffers? [y/N]" }, function(input)
                     if input == 'y' then
                         require("hbac").close_unpinned()
-                        vim.notify("HBAC: closed unpinned buffers")
-                    else
-                        vim.notify("HBAC: operation interrupted")
+                        vim.notify("HBAC: closed untouched buffers")
                     end
                 end)
             end,
-            desc = "HBAC: close all unpinned buffers"
-        }
+            desc = "HBAC: close untouched buffers"
+        },
     },
 
     opts = {
         autoclose = false, -- only close with keybinding
-        -- threshold = 10, -- for autoclose
-        close_command = function(bufnr)
-            -- skip terminal buffers. By default Hbac attempts to close them as they are not pinned automatically.
-            if vim.bo[bufnr].buftype == 'terminal' then
-                return
-            end
-            vim.api.nvim_buf_delete(bufnr, {})
-        end,
         close_buffers_with_windows = false, -- do not close buffers with winodws
-        telescope = {} -- do not use telescope integration
     },
 
 }

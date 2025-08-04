97a5f39748a95f1413413c65e1169932335819d2
Noè Archimede Pezzoli <noearchimede@gmail.com>
2025-08-04 23:17:05 +0200

    window-picker: small config changes

--- lua/plugins/interface/window-picker.lua
+++ lua/plugins/interface/window-picker.lua
@@ -9,30 +9,30 @@ return {
     version = '2.*',
 
     config = {
-        -- 'statusline-winbar' | 'floating-big-letter'
-        hint = 'floating-big-letter',
-        selection_chars = 'FJDKSLAGHTZRUEIWOQPVBCNXMY',
-        show_prompt = true,
+        hint = 'floating-letter',
+        show_prompt = false,
         filter_rules = {
             -- when there is only one window available to pick from, use it without prompting
             autoselect_one = true,
             include_current_win = true,
-            -- filter using buffer options
-            bo = {
-                filetype = {
-                    'NvimTree',
-                    'trouble',
-                    'aerial',
-                    'fidget',
-                    'undotree',
-                    'qf',
+            filter_rules = {
+                -- filter using buffer options
+                bo = {
+                    filetype = {
+                        'NvimTree',
+                        'trouble',
+                        'aerial',
+                        'fidget',
+                        'undotree',
+                        'qf',
+                    },
+                    buftype = {
+                        'terminal',
+                    },
                 },
-                buftype = {
-                    'terminal',
-                },
+                -- filter using window options
+                wo = {},
             },
-            -- filter using window options
-            wo = {},
-        },
+        }
     },
 }

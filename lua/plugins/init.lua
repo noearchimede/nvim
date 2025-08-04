93b57d8853f1fe3b3c5ca61655f3254bdb556b0f
Noè Archimede Pezzoli <noearchimede@gmail.com>
2025-08-05 00:48:53 +0200

    MOVE most plugins into simpler fonder structure

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,17 +1,11 @@
 -- This spec is passed to lazy.setup() and must list all modules in this folder in 'import' fields
 return {
 
-    { import = "plugins.buffers" },
-    { import = "plugins.completion" },
-    { import = "plugins.editing" },
-    { import = "plugins.files" },
+    { import = "plugins.editor" },
     { import = "plugins.filetypes" },
-    { import = "plugins.git" },
     { import = "plugins.interface" },
-    { import = "plugins.lsp" },
     { import = "plugins.navigation" },
     { import = "plugins.tools" },
-    { import = "plugins.syntax" },
-    { import = "plugins.workspace" },
+    { import = "plugins.tmp" },
 
 }

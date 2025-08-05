991fe86bd81a112c3405d03ddf24b214fe7e732d
Noè Archimede Pezzoli <noearchimede@gmail.com>
2025-08-06 00:09:18 +0200

    init: remove unnecessary lines

--- init.lua
+++ init.lua
@@ -10,20 +10,14 @@ vim.g.mapleader = '\\'
 vim.g.maplocalleader = ','
 
 
-
 -- – Plugins ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-
--- disable netrw (nvim-tree is used instead)
--- this needs to be called at the start of the configuration
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 
 -- Plugins managed by lazy-nvim. The actual plugin specs are defined in the
 -- 'plugins' folder, where each plugin or group of plugins is defined in its
 -- own file.
 -- install lazy.nvim (code copied from lazy.nvim documentation)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then ---@diagnostic disable-line: undefined-field
   vim.fn.system({ "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git", "--branch=stable",
     lazypath,
@@ -52,7 +46,3 @@ vim.g.vimrc_keyboard_remappings_path = vim.fn.stdpath("config") .. "/.vimrc_keyb
 if vim.fn.filereadable(vim.fn.expand(vim.g.vimrc_keyboard_remappings_path)) ~= 0 then
     vim.cmd('source ' ..  vim.g.vimrc_keyboard_remappings_path)
 end
-
-
--- – Load neovide config ––––––––––––––––––––––––––––––––––––––––––––––––––––––
-

f637bddb4457e9a9b81d0378ed4c1df5aa7838ab
Noè Archimede Pezzoli <noearchimede@gmail.com>
2025-08-08 13:14:57 +0200

    comment: fix lazy loading, remove defaults redefinitions

--- lua/plugins/editor/comment.lua
+++ lua/plugins/editor/comment.lua
@@ -3,28 +3,9 @@ return {
     'numToStr/comment.nvim',
 
     keys = {
-        { "gc", desc = "Comment: line(s)" },
-        { "gb", desc = "Comment: block" },
+        { "gc", mode = { "n", "v" }, desc = "Comment: line(s)" },
+        { "gb", mode = { "n", "v" }, desc = "Comment: block" },
     },
 
-    opts = {
-        padding = true, ---Add a space b/w comment and the line
-        sticky = true, ---Whether the cursor should stay at its position
-        ignore = nil, ---Lines to be ignored while (un)comment
-        toggler = { ---LHS of toggle mappings in NORMAL mode
-            line = 'gcc', ---Line-comment toggle keymap
-            block = 'gbb', ---Block-comment toggle keymap
-        },
-        opleader = { ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-            line = 'gc', ---Line-comment keymap
-            block = 'gb', ---Block-comment keymap
-        },
-        extra = { ---LHS of extra mappings
-            above = 'gcO', ---Add comment on the line above
-            below = 'gco', ---Add comment on the line below
-            eol = 'gcA', ---Add comment at the end of line
-        },
-        pre_hook = nil, ---Function to call before (un)comment
-        post_hook = nil, ---Function to call after (un)comment
-    }
+    config = true
 }
